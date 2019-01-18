4f88cfef06740cc370250268b6a6dc2322e36826
Lucas Cavalare <lucascavalare@gmail.com>
2019-01-18 20:48:10 -0200

    Updating Long to meet the needs.

--- 02-array-seq/metro_lat_long.py
+++ 02-array-seq/metro_lat_long.py
@@ -9,5 +9,6 @@ metro_areas = [
 print('{:15} | {:^9} | {:^9}'.format('', 'lat.', 'long.'))
 fmt = '{:15} | {:9.4f} | {:9.4f}'
 for name, cc, pop, (latitude, longitude) in metro_areas:  # <2>
-    if longitude <= 0:  # <3>
+    #if longitude <= 0:  # <3>
+    if longitude >= 0: # <3>
         print(fmt.format(name, latitude, longitude))
