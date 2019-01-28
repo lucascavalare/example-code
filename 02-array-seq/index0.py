ced534d06d6359e4e79f9c473581640cb29db67c
Lucas Cavalare <lucascavalare@gmail.com>
2019-01-28 19:15:02 -0200

    Changin in enumerate

--- 02-array-seq/index0.py
+++ 02-array-seq/index0.py
@@ -7,7 +7,7 @@ WORD_RE = re.compile('\w+')
 
 index = {}
 with open(sys.argv[1], encoding ='utf-8') as fp:
-    for line_no, line inenumerate(fp, 1):
+    for line_no, line in enumerate(fp, 1):
         for match in WORD_RE.finditer(line):
             word = match.group()
             column_no = match.start()+1
